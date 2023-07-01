f7770190873f5c5994f0c919641005e68b23fbf4
Josh Karpel <josh.karpel@gmail.com>
2023-06-30 21:00:10 -0500

    more examples

--- examples/02_what_pattern_matching_is_not.py
+++ examples/02_what_pattern_matching_is_not.py
@@ -23,6 +23,9 @@ def describe_list_of_points(points: list[Point]) -> str:
         case [Point(x, y)]:
             return f"A single point {x}, {y} is in the list."
 
+        case [Point(int(), 0) | Point(0, int())]:
+            return "A single point on the X or Y axis is in the list."
+
         case [Point(0, y1), Point(0, y2)]:
             return f"Two points on the Y axis at {y1}, {y2} are in the list."
 
@@ -34,7 +37,8 @@ def describe_list_of_points(points: list[Point]) -> str:
 
 
 print(f"{describe_list_of_points([])=}")
-print(f"{describe_list_of_points([Point(0, 0)])=}")
+print(f"{describe_list_of_points([Point(0, 2)])=}")
+print(f"{describe_list_of_points([Point(1, 2)])=}")
 print(f"{describe_list_of_points([Point(0, 1), Point(0, 4)])=}")
 print(f"{describe_list_of_points([Point(0, 1), Point(0, 4), Point(0, 5)])=}")
 print(f"{describe_list_of_points([Point(0, 1), Point(0, 4), Point(2, 5)])=}")
